5ef8fc36536c3f4fa55b1f35f714648e6a321026
HristijanGigov <119969447+HristijanGigov@users.noreply.github.com>
2023-04-08 08:25:47 +0200

    Update Flask_app.py

--- Flask_app.py
+++ Flask_app.py
@@ -2,18 +2,18 @@ from flask import Flask, request, jsonify
 import sqlite3
 import pymongo
 
-
+#creating flask app
 app = Flask(__name__)
 
 
-
+#forming a route with get method 
 @app.route("/", methods = ["GET"]) 
 def read():
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("data.db") #connection to data base 
     mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM companies LIMIT 10") 
+    mycursor.execute("SELECT * FROM companies LIMIT 10") #read 10 companies 
     companies = []
-    for x in mycursor.fetchall():
+    for x in mycursor.fetchall():#looping through the information
         y = {
             "id":   x[0],
             "name": x[1],
@@ -22,29 +22,29 @@ def read():
             "nace": x[4],
             "website": x[5]
         }
-        companies.append(y) 
-    mycursor.close()
+        companies.append(y) #appending the looped companies
+    mycursor.close() 
     db.close()
     return jsonify({"companies": companies})
 
 
 
-
+# route with post method on mongo db
 @app.route("/", methods=["POST"])
 def add():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")  
+    client = pymongo.MongoClient("mongodb://localhost:27017/")  #connection throught local host
     db = client["mydb"] 
-    lista = db["cleaned_companies"] 
+    lista = db["cleaned_companies"] #forming a new db 
     get_data = request.get_json()
-    append = lista.insert_one(get_data)
+    append = lista.insert_one(get_data)  #appending the data in the new formed db
 
-    
+    #returns failure if the program has errors
     if append == True:
         return "success" 
     else:
         return "failed"
  
 
-
+#activating the debugger on flask
 if __name__ == "__main__": 
     app.run(debug=True)
